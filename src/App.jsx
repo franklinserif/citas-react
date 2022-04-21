14c9413b387bcd8aeef2cbf44f0630d184af74f8
franklin rodriguez <franklinserif@gmail.com>
2022-04-21 17:58:13 -0400

    Header and Form component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import './App.css';
+import Header from './components/Header';
 
-const App = () => {
-  return <></>;
-};
+const App = () => (
+  <>
+    <Header />
+  </>
+);
 
 export default App;
