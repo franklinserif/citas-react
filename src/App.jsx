f55c0424a855379566a1182fda7488d943cbf690
franklin rodriguez <franklinserif@gmail.com>
2022-04-22 23:06:41 -0400

    Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from './components/Header';
+import Form from './components/Form';
+import PatientList from './components/PatientList';
 
 const App = () => (
-  <>
+  <div className="container mx-auto mt-20">
     <Header />
-  </>
+    <div className="mt-12 md:flex">
+      <Form />
+      <PatientList />
+    </div>
+  </div>
 );
 
 export default App;
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,3 @@
+const PatientList = () => <div className="md:w-1/2 lg:w-3/5">PatientList</div>;
+
+export default PatientList;
