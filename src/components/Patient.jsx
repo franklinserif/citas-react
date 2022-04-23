e7ec4ffb2142acdefd1b85009baf8c44a53bbf1e
franklin rodriguez <franklinserif@gmail.com>
2022-04-23 00:07:24 -0400

    eslint-config-prettier added

---
+++ src/components/Patient.jsx
@@ -0,0 +1,32 @@
+const Patient = () => (
+  <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Nombre:
+      <span className="font-normal normal-case"> Hook</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Propietario:
+      <span className="font-normal normal-case"> Franklin</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Email:
+      <span className="font-normal normal-case"> Franklinserif@gmail.com</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Fecha Alta:
+      <span className="font-normal normal-case"> 10/04/12</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Sintomas:
+      <span className="font-normal normal-case">
+        {' '}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque at,
+        molestiae quidem inventore vero vel dolore facere cupiditate
+        consequuntur ea impedit, voluptatum earum accusantium? Esse deserunt
+        alias accusamus totam debitis!
+      </span>
+    </p>
+  </div>
+);
+
+export default Patient;
--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,3 +1,18 @@
-const PatientList = () => <div className="md:w-1/2 lg:w-3/5">PatientList</div>;
+import Patient from './Patient';
+
+const PatientList = () => (
+  <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+    <p className="text-xl mt-5 mb-10 text-center">
+      Administrar tus
+      <span className="text-indigo-600 font-bold"> Pacientes</span>
+    </p>
+    <Patient />
+    <Patient />
+    <Patient />
+    <Patient />
+    <Patient />
+  </div>
+);
 
 export default PatientList;
