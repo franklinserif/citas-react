27ead3bc43e0d1424797c7c7982d8c0d99dbe754
franklin rodriguez <franklinserif@gmail.com>
2022-04-23 21:32:55 -0400

    Proptypes for PatientList props

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,18 +1,18 @@
+import PropTypes from 'prop-types';
 import Patient from './Patient';
 
-const PatientList = () => (
+const PatientList = ({ patients }) => (
   <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
     <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
     <p className="text-xl mt-5 mb-10 text-center">
       Administrar tus
       <span className="text-indigo-600 font-bold"> Pacientes</span>
     </p>
-    <Patient />
-    <Patient />
-    <Patient />
-    <Patient />
-    <Patient />
   </div>
 );
+
+PatientList.propTypes = {
+  patients: PropTypes.node.isRequired,
+};
 
 export default PatientList;
