03294c7ca862d41d18a5f5d305f17da3a2939c87
franklin rodriguez <franklinserif@gmail.com>
2022-04-23 23:44:11 -0400

    added Patients provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import PatientsProvider from './context/PatientsProvider';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PatientsProvider>
+      <App />
+    </PatientsProvider>
   </React.StrictMode>
-)
+);
